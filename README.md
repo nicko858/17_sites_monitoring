9e19a9a6310fc5fda9d3d523d20069b01d09ec1d
nicko858 <ikar2021@hotmail.com>
2018-08-02 17:55:32 +0300

    script&readme&requirements

--- README.md
+++ README.md
@@ -1,6 +1,86 @@
 # Sites Monitoring Utility
+The program checks sites health. At the input - a text file with URLs for verification. At the output, the status of each site, based on the following checks:
+- the server responds to the request with the status of HTTP 200;
+- the domain name of the site is paid for at least 1 month in advance.
 
-[TODO. There will be project description]
+# Quickstart
+
+The program is represented by the module check_sites_health.py. Module check_sites_health.py contains the following functions:
+
+- ```check_file()```
+- ```get_args()```
+- ```get_domain_expiration_date()```
+- ```is_domain_name_payed()```
+- ```is_server_respond_with_200()```
+- ```load_urls4check()```
+- ```print_site_status()```
+
+The program uses these libs from Python standart and third-party libraries:
+
+- ```datetime```
+- ```request```
+- ```argparse```
+- ```pytz```
+- ```path```
+- ```defaultdict from collections```
+- ```whois```
+- ``dateutil``
+
+
+**How in works:**
+
+- The program reads a text file with URLs  
+- Reads content line by line
+- For every url checks server HTTP-status, domain name of the site is paid
+- Prints result check to the console output 
+
+# How to Install
+First, you should make a text file with URLs in such format:
+```http://youtube.com
+http://facebook.com
+http://baidu.com
+http://wikipedia.org
+http://reddit.com
+http://yahoo.com
+```
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
+
+
+# How to run
+- Activate virtualenv
+``` bash
+source <path_to_virtualenv>/bin/activate
+```
+- Run script with virtualenv-interpreter
+```bash
+<path_to_virtualenv>/bin/python3.5 check_sites_health.py
+```
+If everything is fine, you'll see such output:
+```text
+****************************************
+Site - http://youtube.com
+Server is ok - True
+Domain payed till next month - True
+****************************************
+****************************************
+Site - http://facebook.com
+Server is ok - False
+Domain payed till next month - True
+****************************************
+****************************************
+Site - http://baidu.com
+Server is ok - True
+Domain payed till next month - False
+****************************************
+```
+
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,95 @@
-def load_urls4check(path):
-    pass
+import whois
+import requests
+from os.path import isfile
+import argparse
+from requests import HTTPError
+from requests import ConnectionError
+from datetime import datetime
+from dateutil import relativedelta
+from requests import ReadTimeout
+from socket import timeout
+
+
+def check_file(file_path):
+    if not isfile(file_path):
+        message = "'{}' is not a file or doesn't exist".format(file_path)
+        raise argparse.ArgumentTypeError(message)
+    return file_path
+
+
+def get_args():
+    script_usage = "python check_sites_health.py <path to url_list file>"
+    parser = argparse.ArgumentParser(
+        description="How to run check_sites_health.py:",
+        usage=script_usage
+    )
+    parser.add_argument(
+        "site_list",
+        type=check_file,
+        help="Specify the path to url_list file"
+    )
+    args = parser.parse_args()
+    return args
+
+
+def load_urls4check(path_to_site_list):
+    urls4check = []
+    with open(path_to_site_list) as urls_list:
+        for line in urls_list:
+            urls4check.append(line.splitlines())
+    return urls4check
+
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        return True
+    except (ConnectionError, HTTPError, timeout, ReadTimeout):
+        return False
 
-def get_domain_expiration_date(domain_name):
-    pass
 
-if __name__ == '__main__':
-    pass
+def get_domain_expiration_date(url):
+    try:
+        domain_whois = whois.whois(url)
+        return domain_whois.expiration_date
+    except timeout:
+        return None
+
+
+def is_domain_name_payed(expiration_date, months):
+    today = datetime.today()
+    nextmonth = today + relativedelta.relativedelta(months=months)
+    if isinstance(expiration_date, list):
+        return expiration_date[0] >= nextmonth
+    return expiration_date >= nextmonth
+
+
+def print_site_status(url, is_server_ok, is_domain_payed):
+    separator = "*"*40
+    print(separator)
+    print("Site - {}\nServer is ok - {}\n"
+          "Domain paid till next month - {}".format(
+           url,
+           is_server_ok,
+           is_domain_payed
+          ))
+    print(separator)
+
+
+if __name__ == "__main__":
+    args = get_args()
+    path_to_site_list = args.site_list
+    urls4check = load_urls4check(path_to_site_list)
+    for site in urls4check:
+        url = "".join(site)
+        domain_expiration_date = get_domain_expiration_date(url)
+        is_server_ok = is_server_respond_with_200(url)
+        if not domain_expiration_date:
+            is_domain_payed = False
+        else:
+            is_domain_payed = is_domain_name_payed(
+                domain_expiration_date,
+                months=1
+            )
+            print_site_status(url, is_server_ok, is_domain_payed)
